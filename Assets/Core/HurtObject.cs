900d4a563941c8ac1abf7f2f305ae054b24f384a
Mail510 <masafumidoingorigami@yahoo.co.jp>
2020-03-17 21:52:43 +0900

    test

---
+++ Assets/Core/HurtObject.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using System;
+
+[RequireComponent(typeof(Rigidbody))]
+public class HurtObject : MonoBehaviour
+{
+    public int MaxHP;
+    [HideInInspector]
+    public int CurrentHP;
+    public ObjectType type;
+
+
+    public Subject<int> damagEvent = new Subject<int>();
+    public Subject<int> dethEvent = new Subject<int>();
+
+    public IObservable<int> DamageEvent
+    {
+        get { return damagEvent; }
+    }
+    public IObservable<int> DethEvent
+    {
+        get { return dethEvent; }
+    }
+
+    public void Start()
+    {
+        CurrentHP = MaxHP;
+    }
+
+    public void GetDamage(int damage)
+    {
+        CurrentHP -= damage;
+        damagEvent.OnNext(damage);
+        if (CurrentHP<=0)
+        {
+            dethEvent.OnNext(damage);
+        }
+    }
+
+}
+public enum ObjectType
+{
+    Player,Enemy,
+}
---
+++ Assets/Core/Move.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public Vector2 screen = new Vector2(5, 5);
+    public float speed = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 move = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+        move *= Time.deltaTime * speed;
+        Vector3 pos = transform.position;
+        pos += move;
+        pos = new Vector3(Mathf.Clamp(pos.x, -screen.x / 2, screen.x / 2), 0, Mathf.Clamp(pos.z, -screen.y / 2, screen.y / 2));
+        transform.position = pos;
+    }
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(Vector3.zero,new Vector3(screen.x,0,screen.y));
+    }
+}
