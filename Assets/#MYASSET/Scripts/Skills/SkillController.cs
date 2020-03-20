60de20008bd5df8a73dfceda8b3f1eef86675cce
そな <lite01250@gmail.com>
2020-03-20 22:41:20 +0900

    SkillCpntrollerにPlayの関数を作成

--- Assets/#MYASSET/Scripts/Skills/SkillController.cs
+++ Assets/#MYASSET/Scripts/Skills/SkillController.cs
@@ -1,34 +1,64 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UniRx;
 
 public class SkillController : MonoBehaviour
 {
     private IInputProvider InputProvider;
 
-    public ActiveSkill NormalShotSkill { get; private set; }
+    public ActiveSkill NormalShotSkill { get; set; }
 
-    public List<ActiveSkill> ActiveSkills { get; private set; }
+    public List<ActiveSkill> ActiveSkills { get;  set; }
 
-    public List<PassiveSkill> PassiveSkills { get; private set; }
+    public List<PassiveSkill> PassiveSkills { get; set; }
 
     void Start()
     {
-        
+        InputProvider = GameObject.FindGameObjectWithTag("GameManager").GetComponent<IInputProvider>();
+
+        InputProvider.NormalShotButtonPushed
+            .Where(_ => _)
+            .Subscribe(_ => PlayNormalShotSKill());
+
+        InputProvider.Skill1ButtonPushed
+            .Where(_ => _)
+            .Subscribe(_ => PlayAstiveSkill(0));
+
+        PlayPassiveSills();
     }
 
-    public void ChangeSkill()
+    private void PlayNormalShotSKill()
     {
+        NormalShotSkill.PlaySkill();
+    }
+
+    private void PlayAstiveSkill(int num)
+    {
+        var playable = true;
+
+        if (NormalShotSkill.IsRunning.Value)
+            playable =  false;
+
+        foreach (var activeSkill in ActiveSkills)
+        {
+            if (activeSkill.IsRunning.Value)
+                playable = false;
+        }
+
+        if (playable)
+            ActiveSkills[num].PlaySkill();
 
     }
 
-    public void RemoveSkill()
+    private void PlayPassiveSills()
     {
-
-    }
-
-    public void AddSkill()
-    {
-
+        foreach (var passiveSkill in PassiveSkills)
+        {
+            if (!passiveSkill.IsRunning.Value)
+            {
+                passiveSkill.PlaySkill();
+            }
+        }
     }
 }
