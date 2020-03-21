01518928005626006482647bb1e3074f42413152
そな <lite01250@gmail.com>
2020-03-21 18:50:49 +0900

    Character関連のスクリプトを整理

--- Assets/#MYASSET/Scripts/Player/PlayerState.cs
+++ Assets/#MYASSET/Scripts/Player/PlayerState.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChatacterState : MonoBehaviour
+public class PlayerState : MonoBehaviour, ICharacterState
 {
     // Start is called before the first frame update
     void Start()
