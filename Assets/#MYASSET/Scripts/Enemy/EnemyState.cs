e798dc6239af97aa75be63236f62baf33e337e99
そな <lite01250@gmail.com>
2020-03-21 20:49:20 +0900

    EnemyStateの変数がインスペクターで表示されるように変更

--- Assets/#MYASSET/Scripts/Enemy/EnemyState.cs
+++ Assets/#MYASSET/Scripts/Enemy/EnemyState.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class EnemyState : MonoBehaviour, ICharacterState
 {
-    public Parameter CurrentHP { get; } = new Parameter(10);
+    public Parameter CurrentHP => _CurrentHP;
+    [SerializeField] private Parameter _CurrentHP = new Parameter(10);
 
-    public Parameter MaxHP { get; } = new Parameter(10);
+    public Parameter MaxHP => _MaxHP;
+    [SerializeField] private Parameter _MaxHP = new Parameter(10);
 
-    public Parameter Barrier { get; } = new Parameter(0);
+    public Parameter Barrier => _Barrier;
+    [SerializeField] private Parameter _Barrier = new Parameter(0);
 
-    public Parameter ATK { get; } = new Parameter(1);
+    public Parameter ATK => _ATK;
+    [SerializeField] private Parameter _ATK = new Parameter(1);
 
-    public Parameter CoolTime { get; } = new Parameter(1);
+    public Parameter CoolTime => _CoolTime;
+    [SerializeField] private Parameter _CoolTime = new Parameter(1);
 }
