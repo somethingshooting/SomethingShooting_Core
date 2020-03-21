38e224cd441a6bc5d5fd3e58d983797525c6f9ce
そな <lite01250@gmail.com>
2020-03-22 00:55:01 +0900

    BulletBehaviourとIBulletを作成

---
+++ Assets/#MYASSET/Scripts/Bullet/BulletBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BulletBehaviour : MonoBehaviour, IBullet
+{
+    public Parameter MoveSpeed = new Parameter(1);
+
+    public Parameter ATK = new Parameter(1);
+
+    public SkillAttributeType AttributeType;
+
+    public void OnDestroy()
+    {
+        Destroy(gameObject);
+    }
+
+    protected virtual void OnTriggerEnter(Collider other)
+    {
+        switch (other.tag)
+        {
+            case "Player":
+                if (gameObject.tag == "EnemyBullet")
+                {
+                    other.GetComponent<IHitPointObject>()
+                        .GetDamage(ATK.Value, AttributeType);
+                }
+                break;
+            case "Enemy":
+                if (gameObject.tag == "PlayerBullet")
+                {
+                    other.GetComponent<IHitPointObject>()
+                        .GetDamage(ATK.Value, AttributeType);
+                }
+                break;
+            default:
+                break;
+        }
+    }
+}
