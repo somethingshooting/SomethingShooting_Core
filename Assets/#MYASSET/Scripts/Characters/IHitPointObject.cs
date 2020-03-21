23d6eb935c100ac7d8208823314d5007510bf8bb
Mail510 <masafumidoingorigami@yahoo.co.jp>
2020-03-21 19:10:50 +0900

    エラーあるよ！

--- Assets/#MYASSET/Scripts/Characters/IHitPointObject.cs
+++ Assets/#MYASSET/Scripts/Characters/IHitPointObject.cs
@@ -1,8 +1,11 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UniRx;
 
 public interface IHitPointObject
 {
-
+    IObservable<Unit> DeadSubject { get; }
+    void GetDamage(int value, SkillAttributeType attribute);
 }
