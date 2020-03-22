d94beee53b1269b738d595459002952ad901224a
Mail510 <masafumidoingorigami@yahoo.co.jp>
2020-03-22 21:13:01 +0900

    ダメージ処理

--- Assets/#MYASSET/Scripts/Player/PlayerHitPoint.cs
+++ Assets/#MYASSET/Scripts/Player/PlayerHitPoint.cs
@@ -1,18 +1,26 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UniRx;
 
-public class PlayerHitPoint : MonoBehaviour
+public class PlayerHitPoint : MonoBehaviour,IHitPointObject
 {
-    // Start is called before the first frame update
-    void Start()
+    public IObservable<Unit> DeadSubject => _DeadSubject;
+    protected Subject<Unit> _DeadSubject;
+
+    public virtual void GetDamage(int value, SkillAttributeType attribute)
     {
-        
+        _State.HP.AddValue(-value);
+        if (_State.HP.Value<=0)
+        {
+            _DeadSubject.OnNext(Unit.Default);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private PlayerState _State;
+    private void Start()
     {
-        
+        _State = GetComponent<PlayerState>();
     }
 }
