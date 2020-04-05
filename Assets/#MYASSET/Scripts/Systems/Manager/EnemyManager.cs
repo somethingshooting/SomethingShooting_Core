f3500aa13f52c4504c77208fceecd6f1ae0ee2ff
そな <lite01250@gmail.com>
2020-04-05 14:16:12 +0900

    EnemyBehaviourの作成

---
+++ Assets/#MYASSET/Scripts/Systems/Manager/EnemyManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : ManagerBase<EnemyManager>
+{
+    public List<EnemyBehaviour> EnemyList = new List<EnemyBehaviour>();
+
+    private void Start()
+    {
+        foreach (var enm in GameObject.FindGameObjectsWithTag("Enemy"))
+        {
+            EnemyList.Add(enm.GetComponent<EnemyBehaviour>());
+        }
+    }
+}
