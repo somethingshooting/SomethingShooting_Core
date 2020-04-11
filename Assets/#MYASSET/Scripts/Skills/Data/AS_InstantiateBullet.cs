48d9d88e06d73ea7f1f4e257d5ca82f6fff7479e
Mail510 <masafumidoingorigami@yahoo.co.jp>
2020-04-11 12:03:19 +0900

    activeskill

---
+++ Assets/#MYASSET/Scripts/Skills/Data/AS_InstantiateBullet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AS_Instance : ActiveSkill
+{
+    [SerializeField] private GameObject _Bullet;
+
+    protected override void Init()
+    {
+
+    }
+
+    protected override void SkillStart()
+    {
+        Instantiate(_Bullet);
+    }
+
+    protected override void SkillUpdate()
+    {
+
+    }
+}
