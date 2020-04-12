cc8061b4e455214c703a78c8b85d235ce6a74417
Mail510 <masafumidoingorigami@yahoo.co.jp>
2020-04-12 14:29:01 +0900

    エフェクト導入

--- Assets/#MYASSET/Scripts/Bullet/Data/Bl_GoStraight.cs
+++ Assets/#MYASSET/Scripts/Bullet/Data/Bl_GoStraight.cs
@@ -4,12 +4,10 @@ using UnityEngine;
 
 public class Bl_GoStraight : BulletBehaviour
 {
-    [SerializeField]
-    private float _Speed = 1;
 
     private void Update()
     {
-        transform.position += Vector3.forward * _Speed;
+        transform.Translate(Vector3.forward * MoveSpeed);
     }
 
 }
--- Assets/#MYASSET/Scripts/Skills/Data/AS_InstantiateBullet.cs
+++ Assets/#MYASSET/Scripts/Skills/Data/AS_InstantiateBullet.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 public class AS_InstantiateBullet : ActiveSkill
 {
     [SerializeField] private GameObject _Bullet;
-
+    [SerializeField] private Transform _ShotTransform;
     protected override void Init()
     {
-
+        if (_ShotTransform == null)
+        {
+            _ShotTransform = transform;
+        }
     }
 
     protected override void SkillStart()
     {
        var obj =  Instantiate(_Bullet);
-        obj.transform.position = transform.position;
+        obj.transform.position = _ShotTransform.position;
     }
 
     protected override void SkillUpdate()
