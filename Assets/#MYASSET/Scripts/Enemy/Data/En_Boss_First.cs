504c3273b64f14407e7fedd63df5d5e432562a98
そな <lite01250@gmail.com>
2020-04-12 15:54:35 +0900

    EnemyBossのテスト用シーンを作成

---
+++ Assets/#MYASSET/Scripts/Enemy/Data/En_Boss_First.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class En_Boss_First : EnemyBehaviour
+{
+    [SerializeField] private List<Vector3> _CheckPoint = new List<Vector3>();
+
+    [SerializeField] private float _MoveSpeed = 1.0f;
+
+    [SerializeField] private int _Phase = 0;
+
+    protected override void Init()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected void Update()
+    {
+        
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        foreach (var pos in _CheckPoint)
+        {
+            Gizmos.DrawSphere(pos, 0.1f);
+        }
+    }
+}
