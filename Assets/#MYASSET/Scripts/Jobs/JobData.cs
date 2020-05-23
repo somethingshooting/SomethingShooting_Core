82007be6f35600d64cae35dacd4f590510e7c6af
そな <lite01250@gmail.com>
2020-05-23 23:18:48 +0900

    Job関係のコンポーネントのがわだけ作成

---
+++ Assets/#MYASSET/Scripts/Jobs/JobData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(menuName = "CreateJobData")]
+public class JobData : ScriptableObject
+{
+    public string Name;
+    public string Flaver;
+    public List<JobData> PrerequisiteJob;
+    public List<SkillData> GivenSkill;
+    public StoryFlag Storyflag;
+
+    [Serializable]
+    public class StoryFlag
+    {
+        public int Ambition, Harmony, Intellect;
+    }
+}
