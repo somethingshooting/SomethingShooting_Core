5db0792672f651975d160c71ac2669e84eed4b39
Mail510 <masafumidoingorigami@yahoo.co.jp>
2020-06-07 21:30:46 +0900

    ParagraphManagerを制作

---
+++ Assets/#MYASSET/Scripts/Systems/ParagraphManager/ParagraphMaker.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+
+public class ParagraphMaker : MonoBehaviour
+{
+}
+#endif
--- Assets/#MYASSET/Scripts/Systems/ParagraphManager/ParagraphManager.cs
+++ Assets/#MYASSET/Scripts/Systems/ParagraphManager/ParagraphManager.cs
@@ -3,81 +3,231 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEditor;
+using UniRx;
 
+[RequireComponent(typeof(TextEventUI))]
 public class ParagraphManager : MonoBehaviour
 {
-    public ParagraphData[] Story;
-    // Start is called before the first frame update
-    void Awake()
-    {
-        foreach (var paradata in Story)
-        {
-            for (int i = 0; i < paradata.Events.Length; i++)
-            {
-               
-            }
-        }
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    public BattleEvent obj;
-    public string json;
-    [ContextMenu("jsontest")]
-    public void jsontest()
-    {
-        json = JsonUtility.ToJson(obj);
-    }
-
-    //データクラスの定義
-    public class ParagraphData
-    {
-      public  ParagraphEvent[] Events;
-    }
-    public abstract class ParagraphEvent
-    {
-        public Type Type;
-    }
-    [Serializable]
-    public class BattleEvent : ParagraphEvent
+    [SerializeField] private ParagraphData[] MainStory;
+    [SerializeField] private ParagraphData[] BranchStory;
+    [SerializeField] private ParagraphData ComplexStory;
+    public int[] StoryParamater;
+    public class ParagraphData:ScriptableObject
     {
         public int FlagCount;
-        public EnemyData[] Enemies;
-        [Serializable]
-        public class EnemyData
+      //  public CharacterData[] Characters;
+        public EnemyBlock[] EnemieBlocks;
+        public BossBlock[] BossBlocks;
+        public TextBlock[] TextBlocks;
+        public JobSelectBlock[] JobSelectBlocks;
+        public abstract class EventBlock
         {
             public Vector3 Position;
-            public GameObject Prefab;
-            public bool Flag;
         }
-        //
-        public TextAsset Text;
-    }
-    [Serializable]
-    public class TextEvent : ParagraphEvent
-    {
-        public CharacterData[] Characters;
-        public TextData[] Texts;
+        [Serializable]
+        public class EnemyBlock:EventBlock
+        {
+            public GameObject Prefab;
+        }
+        [Serializable]
+        public class BossBlock : EventBlock
+        {
+            public GameObject Prefab;
+        }
+        [Serializable]
+        public class TextBlock:EventBlock
+        {
+            public CharacterData[] Characters;
+            public TextData[] Texts;
+            [Serializable]
+            public class TextData
+            {
+                public int Character;
+                public string Message;
+            }
+        }
         [Serializable]
         public class CharacterData
         {
             public string Name;
-            public Image Image;
+            public Sprite Image;
         }
         [Serializable]
-        public class TextData
+        public class JobSelectBlock:EventBlock
         {
-            public int Character;
-            public string Message;
         }
-        //
-        public TextAsset Text;
     }
-    [Serializable]
-    public class JobSelectEvent : ParagraphEvent
+      private bool _isMoving = true;
+     private float _CurrentDistance;
+    [SerializeField] private float _ScrollSpeed = 0.5f;
+    [SerializeField] private float _SpownZ = 5;
+
+    private void Awake()
     {
+        CurrentParagraph = MainStory[0];
+        textUI = GetComponent<TextEventUI>();
+    }
+
+    private ParagraphData CurrentParagraph;
+    private int _EnemyCount = 0,_BossCount = 0, _TextCount = 0, _JobSelectCout = 0;
+    private bool _EnemyEnd = false,_BossEnd = false, _TextEnd = false, _JobSelectEnd = false;
+    private void Update()
+    {
+        if (CurrentParagraph == null)
+            return;
+        if (_isMoving)
+        {
+            _CurrentDistance += _ScrollSpeed * Time.deltaTime;
+        }
+        if (!_EnemyEnd)
+        {
+            if (CurrentParagraph.EnemieBlocks.Length > _EnemyCount)
+            {
+                for (int i = _EnemyCount; i < CurrentParagraph.EnemieBlocks.Length; i++)
+                {
+                    if (_CurrentDistance >= CurrentParagraph.EnemieBlocks[i].Position.z)
+                    {
+                        EnemyBlockFunction(CurrentParagraph.EnemieBlocks[i]);
+                        _EnemyCount++;
+                    }
+                    else
+                        return;
+                }
+            }
+            else
+                _EnemyEnd = true;
+        }
+        if (!_BossEnd&& _isMoving)
+        {
+            if (CurrentParagraph.BossBlocks.Length > _BossCount)
+            {
+                for (int i = _BossCount; i < CurrentParagraph.BossBlocks.Length; i++)
+                {
+                    if (_CurrentDistance >= CurrentParagraph.BossBlocks[i].Position.z)
+                    {
+                        BossBlockFunction(CurrentParagraph.BossBlocks[i]);
+                        _BossCount++;
+                    }
+                    else
+                        return;
+                }
+            }
+            else
+                _BossEnd = true;
+        }
+        if (!_TextEnd && _isMoving)
+        {
+            if (CurrentParagraph.TextBlocks.Length > _TextCount)
+            {
+                if (_CurrentDistance >= CurrentParagraph.TextBlocks[_TextCount].Position.z)
+                {
+                    TextBlockFunction(CurrentParagraph.TextBlocks[_TextCount]);
+                    _TextCount++;
+                }
+            }
+            else
+                _TextEnd = true;
+        }
+        if (!_JobSelectEnd && _isMoving)
+        {
+            if (CurrentParagraph.JobSelectBlocks.Length > _JobSelectCout)
+            {
+                if (_CurrentDistance >= CurrentParagraph.JobSelectBlocks[_JobSelectCout].Position.z)
+                {
+                    JobSelectBlockFunction(CurrentParagraph.JobSelectBlocks[_JobSelectCout]);
+                    _JobSelectCout++;
+                }
+            }
+            else
+                _JobSelectEnd = true;
+        }
+        if (_EnemyEnd &&_BossEnd&& _TextEnd && _JobSelectEnd)
+            NextParagraph();
+    }
+    private int _ParagraphProgress = 0;
+    private void NextParagraph()
+    {
+        _ParagraphProgress++;
+        _EnemyCount = 0; _BossCount = 0; _TextCount = 0; _JobSelectCout = 0;
+        _EnemyEnd = false; _BossEnd = false; _TextEnd = false; _JobSelectEnd = false;
+        if (_ParagraphProgress<MainStory.Length)
+        {
+            CurrentParagraph = MainStory[_ParagraphProgress];
+        }else if(_ParagraphProgress == MainStory.Length)
+        {
+            bool isEqual = false;
+            int maxnum = 0;
+            for (int i = 1; i < BranchStory.Length; i++)
+            {
+                if (StoryParamater[i] > StoryParamater[maxnum])
+                    maxnum = i;
+                else if (StoryParamater[i] == StoryParamater[maxnum])
+                    isEqual = true;return;
+            }
+            if (isEqual)
+                CurrentParagraph = ComplexStory;
+            else
+                CurrentParagraph = BranchStory[maxnum];
+        }
+        else
+        {
+            CurrentParagraph = null;
+            //クリア処理
+        }
+    }
+    //-----------------------------------------------------------------------------------------------------------------------
+    private void EnemyBlockFunction(ParagraphData.EnemyBlock block)
+    {
+        var obj = Instantiate(block.Prefab);
+        Vector3 pos = block.Position;
+        pos.z = _SpownZ;
+        obj.transform.position = pos;
+    }
+    private int _BossLeft = 0;
+    private void BossBlockFunction(ParagraphData.BossBlock block)
+    {
+        _isMoving = false;
+        var obj = Instantiate(block.Prefab);
+        Vector3 pos = block.Position;
+        pos.z = _SpownZ;
+        obj.transform.position = pos;
+        obj.GetComponent<EnemyBehaviour>().DeadSubject.Subscribe(_ => { _BossLeft--; if (_BossLeft <= 0) { _isMoving = true; } });
+    }
+    private TextEventUI textUI;
+    private void TextBlockFunction(ParagraphData.TextBlock block)
+    {
+        _isMoving = false;
+        textUI.StartTextEvent(block);
+    }
+    public void TextEventEnd()
+    {
+        _isMoving = true;
+    }
+    [SerializeField] private GameObject JobPanel;
+    [SerializeField] private GameObject SkillPanel;
+    private void JobSelectBlockFunction(ParagraphData.JobSelectBlock block)
+    {
+        _isMoving = false;
+        JobPanel.SetActive(true);
+    }
+    public void JobSelectEnd()
+    {
+        JobPanel.SetActive(false);
+        SkillPanel.SetActive(true);
+    }
+    public void SkillSelectEnd()
+    {
+        SkillPanel.SetActive(false);
+        _isMoving = true;
+    }
+    //------------------------------------------------------------------------------------------------------------------------
+    [SerializeField] private string name;
+    [ContextMenu("CreateParagraph")]
+    private void CreateParagraphData()
+    {
+        var data = ScriptableObject.CreateInstance(typeof(ParagraphData)) as ParagraphData;
+        AssetDatabase.CreateAsset(data, "Assets/#MYASSET/Scripts/Systems/ParagraphManager/" + name + ".asset");
     }
 }
