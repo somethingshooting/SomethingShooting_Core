75d0de5612ce0f2d7d0f6dc67139ec0a59db937e
そな <lite01250@gmail.com>
2020-06-12 23:43:25 +0900

    InputMapにBasicSystemを追加

--- Assets/#MYASSET/Scripts/Systems/Inputs/Data/AlphaInputMap.cs
+++ Assets/#MYASSET/Scripts/Systems/Inputs/Data/AlphaInputMap.cs
@@ -128,7 +128,7 @@ public class @AlphaInputMap : IInputActionCollection, IDisposable
             ""id"": ""c0deff1a-91d8-4f00-9031-132bbff709b6"",
             ""actions"": [
                 {
-                    ""name"": ""NextText"",
+                    ""name"": ""Next"",
                     ""type"": ""Button"",
                     ""id"": ""94110cae-7c2e-4bcc-af94-d81c869bee89"",
                     ""expectedControlType"": ""Button"",
@@ -144,7 +144,72 @@ public class @AlphaInputMap : IInputActionCollection, IDisposable
                     ""interactions"": """",
                     ""processors"": """",
                     ""groups"": """",
-                    ""action"": ""NextText"",
+                    ""action"": ""Next"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        },
+        {
+            ""name"": ""BasicSystem"",
+            ""id"": ""4137c9cf-3717-4e79-b4e7-e6a736128a1c"",
+            ""actions"": [
+                {
+                    ""name"": ""Back"",
+                    ""type"": ""Button"",
+                    ""id"": ""29ba6918-44c8-44e4-9a4c-b83f3ce28a09"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Okay"",
+                    ""type"": ""Button"",
+                    ""id"": ""dda18db6-cc67-4136-bff0-0e13ec5a9562"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Menu"",
+                    ""type"": ""Button"",
+                    ""id"": ""cccc3d77-6d5d-466d-8204-8113998f555c"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""5003a3ea-a003-4b75-9b11-9e585bea1b09"",
+                    ""path"": """",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Back"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""59c633ba-19e1-4056-87a5-5943588853cf"",
+                    ""path"": """",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Okay"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""872f44c9-2bff-48de-bd3d-c106a93ab915"",
+                    ""path"": """",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Menu"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
                 }
@@ -160,7 +225,12 @@ public class @AlphaInputMap : IInputActionCollection, IDisposable
         m_Battle_ActiveSkill_1 = m_Battle.FindAction("ActiveSkill_1", throwIfNotFound: true);
         // Story
         m_Story = asset.FindActionMap("Story", throwIfNotFound: true);
-        m_Story_NextText = m_Story.FindAction("NextText", throwIfNotFound: true);
+        m_Story_Next = m_Story.FindAction("Next", throwIfNotFound: true);
+        // BasicSystem
+        m_BasicSystem = asset.FindActionMap("BasicSystem", throwIfNotFound: true);
+        m_BasicSystem_Back = m_BasicSystem.FindAction("Back", throwIfNotFound: true);
+        m_BasicSystem_Okay = m_BasicSystem.FindAction("Okay", throwIfNotFound: true);
+        m_BasicSystem_Menu = m_BasicSystem.FindAction("Menu", throwIfNotFound: true);
     }
 
     public void Dispose()
@@ -259,12 +329,12 @@ public class @AlphaInputMap : IInputActionCollection, IDisposable
     // Story
     private readonly InputActionMap m_Story;
     private IStoryActions m_StoryActionsCallbackInterface;
-    private readonly InputAction m_Story_NextText;
+    private readonly InputAction m_Story_Next;
     public struct StoryActions
     {
         private @AlphaInputMap m_Wrapper;
         public StoryActions(@AlphaInputMap wrapper) { m_Wrapper = wrapper; }
-        public InputAction @NextText => m_Wrapper.m_Story_NextText;
+        public InputAction @Next => m_Wrapper.m_Story_Next;
         public InputActionMap Get() { return m_Wrapper.m_Story; }
         public void Enable() { Get().Enable(); }
         public void Disable() { Get().Disable(); }
@@ -274,20 +344,69 @@ public class @AlphaInputMap : IInputActionCollection, IDisposable
         {
             if (m_Wrapper.m_StoryActionsCallbackInterface != null)
             {
-                @NextText.started -= m_Wrapper.m_StoryActionsCallbackInterface.OnNextText;
-                @NextText.performed -= m_Wrapper.m_StoryActionsCallbackInterface.OnNextText;
-                @NextText.canceled -= m_Wrapper.m_StoryActionsCallbackInterface.OnNextText;
+                @Next.started -= m_Wrapper.m_StoryActionsCallbackInterface.OnNext;
+                @Next.performed -= m_Wrapper.m_StoryActionsCallbackInterface.OnNext;
+                @Next.canceled -= m_Wrapper.m_StoryActionsCallbackInterface.OnNext;
             }
             m_Wrapper.m_StoryActionsCallbackInterface = instance;
             if (instance != null)
             {
-                @NextText.started += instance.OnNextText;
-                @NextText.performed += instance.OnNextText;
-                @NextText.canceled += instance.OnNextText;
+                @Next.started += instance.OnNext;
+                @Next.performed += instance.OnNext;
+                @Next.canceled += instance.OnNext;
             }
         }
     }
     public StoryActions @Story => new StoryActions(this);
+
+    // BasicSystem
+    private readonly InputActionMap m_BasicSystem;
+    private IBasicSystemActions m_BasicSystemActionsCallbackInterface;
+    private readonly InputAction m_BasicSystem_Back;
+    private readonly InputAction m_BasicSystem_Okay;
+    private readonly InputAction m_BasicSystem_Menu;
+    public struct BasicSystemActions
+    {
+        private @AlphaInputMap m_Wrapper;
+        public BasicSystemActions(@AlphaInputMap wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Back => m_Wrapper.m_BasicSystem_Back;
+        public InputAction @Okay => m_Wrapper.m_BasicSystem_Okay;
+        public InputAction @Menu => m_Wrapper.m_BasicSystem_Menu;
+        public InputActionMap Get() { return m_Wrapper.m_BasicSystem; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(BasicSystemActions set) { return set.Get(); }
+        public void SetCallbacks(IBasicSystemActions instance)
+        {
+            if (m_Wrapper.m_BasicSystemActionsCallbackInterface != null)
+            {
+                @Back.started -= m_Wrapper.m_BasicSystemActionsCallbackInterface.OnBack;
+                @Back.performed -= m_Wrapper.m_BasicSystemActionsCallbackInterface.OnBack;
+                @Back.canceled -= m_Wrapper.m_BasicSystemActionsCallbackInterface.OnBack;
+                @Okay.started -= m_Wrapper.m_BasicSystemActionsCallbackInterface.OnOkay;
+                @Okay.performed -= m_Wrapper.m_BasicSystemActionsCallbackInterface.OnOkay;
+                @Okay.canceled -= m_Wrapper.m_BasicSystemActionsCallbackInterface.OnOkay;
+                @Menu.started -= m_Wrapper.m_BasicSystemActionsCallbackInterface.OnMenu;
+                @Menu.performed -= m_Wrapper.m_BasicSystemActionsCallbackInterface.OnMenu;
+                @Menu.canceled -= m_Wrapper.m_BasicSystemActionsCallbackInterface.OnMenu;
+            }
+            m_Wrapper.m_BasicSystemActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Back.started += instance.OnBack;
+                @Back.performed += instance.OnBack;
+                @Back.canceled += instance.OnBack;
+                @Okay.started += instance.OnOkay;
+                @Okay.performed += instance.OnOkay;
+                @Okay.canceled += instance.OnOkay;
+                @Menu.started += instance.OnMenu;
+                @Menu.performed += instance.OnMenu;
+                @Menu.canceled += instance.OnMenu;
+            }
+        }
+    }
+    public BasicSystemActions @BasicSystem => new BasicSystemActions(this);
     public interface IBattleActions
     {
         void OnPlayerMove(InputAction.CallbackContext context);
@@ -296,6 +415,12 @@ public class @AlphaInputMap : IInputActionCollection, IDisposable
     }
     public interface IStoryActions
     {
-        void OnNextText(InputAction.CallbackContext context);
+        void OnNext(InputAction.CallbackContext context);
+    }
+    public interface IBasicSystemActions
+    {
+        void OnBack(InputAction.CallbackContext context);
+        void OnOkay(InputAction.CallbackContext context);
+        void OnMenu(InputAction.CallbackContext context);
     }
 }
